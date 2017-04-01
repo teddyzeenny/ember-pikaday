48b20afc4186f472006761b76f2e71f65194eeae
Teddy Zeenny <teddyzeenny@gmail.com>
2017-04-01 15:42:39 +0300

    Let app choose pikaday assets

--- index.js
+++ index.js
@@ -2,16 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-pikaday',
-  
-  options: {
-    nodeAssets: {
-      pikaday: {
-        import: [
-          { enabled: process.env.EMBER_CLI_FASTBOOT !== 'true', path: 'pikaday.js' },
-          'css/pikaday.css'
-        ]
-      }
-    }
-  }
+  name: 'ember-pikaday'
 };
